b433e00589f93ef7c8dac9ba7ec1b7716aafbe9a
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-05 02:03:44 +0200

    updated boilerplate

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-btn>Hello Sass Changes</v-btn>
+    <div class="bg-red-200">Fire and magic</div>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   </div>
 </template>

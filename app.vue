2578ea81286752d15e9a6aa30c6252e8e0ac8585
JuperoDev <136336036+JuperoDev@users.noreply.github.com>
2023-08-05 02:06:30 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <v-btn>Hello Sass Changes</v-btn>
-    <div class="bg-red-200">Fire and magic</div>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+Boilerplate for Nuxt3-vuetify-Tailwind
   </div>
 </template>
